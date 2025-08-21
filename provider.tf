ace7bc7ef541543bde7536f8bf6383e3320ad707
Vennela-123-dev <your.vennelakata504@gmail.com>
2025-08-21 09:50:51 +0530

    add provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform{
+  required_providers{
+    aws={
+      source="hashicorp/aws"
+      version="~> 6.0"
+
+    }
+  }
+}
+
+
 provider "aws" {
   region = "eu-west-3" # Change if needed
 }
